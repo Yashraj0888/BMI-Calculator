b6b5f26e50824a9449ae66cd3279d1d697cdbe94
Yashraj0888 <bloodymoonknite888@gmail.com>
2023-11-21 01:42:11 +0530

    bmi

---
+++ script.js
@@ -0,0 +1,38 @@
+const btn=document.querySelector('.btn');
+const result=document.querySelector('.result')
+const image=document.querySelector('.image')
+const comp=document.querySelector('.compliment')
+
+
+btn.addEventListener('click',(e)=>{
+    e.preventDefault();
+    
+    const height=parseInt(document.querySelector('#height').value);
+    const weight=parseInt(document.querySelector('#weight').value);
+    const bmi=parseInt((weight/((height*height)/10000)))
+    result.innerHTML=`<span><h3>${bmi}<h3></span>`
+    if(bmi<=18.5){
+        comp.innerHTML=`<h2>Your are Underweight <br> Go eat something</h2>`
+        let img = document.createElement("img");
+        img.src = "underweight.png";
+        image.appendChild(img);
+    }
+    else if(bmi>=18.5 && bmi<=24.9){
+        comp.innerHTML=`<h2>Your are Normal <br> You are perfect</h2>`
+        let img = document.createElement("img");
+        img.src = "normal.png";
+        image.appendChild(img);
+    }
+    else if(bmi>=25 && bmi<=29.9){
+        comp.innerHTML=`<h2>Your are OverWeight <br> Now time to go to gym</h2>`
+        let img = document.createElement("img");
+        img.src = "overweight.png";
+        image.appendChild(img);
+    }
+    else if(bmi>30){
+        comp.innerHTML=`<h2>Your are Obese <br> For god sake Stop eating</h2>`
+        let img = document.createElement("img");
+        img.src = "fat.png";
+        image.appendChild(img);
+    }
+})
